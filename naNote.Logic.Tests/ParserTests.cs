f1ff0fb6efa686922474899a7c97c28267e2cfd6
CiodenDarkeye <Falcon2001@gmail.com>
2021-01-15 12:55:46 -0800

    Update ParserTests.cs
    Updated ParserTests to be more accurate, and also fixed indentations.

--- naNote.Logic.Tests/ParserTests.cs
+++ naNote.Logic.Tests/ParserTests.cs
@@ -13,42 +13,42 @@ namespace naNote.Logic.Tests
         [Fact]
         public void TestParsingIsWorking()
         {
-        //Given
-        Catalog catalog = new Catalog();
-        string testAction = "ridiculous";
-        string testCategory = "#Work";
-        string testPayload = "What a week!";
-        string parserPass = $"{testAction} {testCategory} {testPayload}";
+            //Given
+            Catalog catalog = new Catalog();
+            string testAction = "ridiculous";
+            string testCategory = "#Work";
+            string testPayload = "What a week!";
+            string parserPass = $"{testAction} {testCategory} {testPayload}";
 
-        //When
-        catalog.CategoryList.Add(new Category(){Name="Work"});
-        var testParser = new Parser(parserPass, catalog);
-        int testCategoryId = catalog.CategoryList.First(p => p.Name == "Work").Id;
+            //When
+            catalog.CategoryList.Add(new Category() { Name = "Work" });
+            var testParser = new Parser(parserPass, catalog);
+            int testCategoryId = catalog.CategoryList.First(p => p.Name == "Work").Id;
 
-        //Then
+            //Then
 
-        // Check that the inputted action matches the outputted action
-        Assert.Equal(testParser.Action, testAction);
-        // Check that the category matches
-        Assert.Contains(catalog.CategoryList.FirstOrDefault(p => p.Name == "Work").Id, testParser.Categories);
-        // Check that the payload works as well
-        Assert.Equal(testParser.Payload, testPayload);
+            // Check that the inputted action matches the outputted action
+            Assert.Equal(testParser.Action, testAction);
+            // Check that the category matches
+            Assert.Contains(catalog.CategoryList.FirstOrDefault(p => p.Name == "Work").Id, testParser.Categories);
+            // Check that the payload works as well
+            Assert.Equal(testParser.Payload, testPayload);
         }
-        
+
         [Fact]
         public void HashesAreStripped()
         {
-        //This tests whether or not hashes are stripped from stored category names.
-        //Given
-        Catalog catalog = new Catalog();
-        string parserPass = "diary #help wow, what a crazy week this has been! #wow #worldofwarcraft";
+            //This tests whether or not hashes are stripped from stored category names.
+            //Given
+            Catalog catalog = new Catalog();
+            string parserPass = "diary #help wow, what a crazy week this has been! #wow #worldofwarcraft";
 
-        //When
-        catalog.CategoryList.Add(new Category(){Name="Work"});
-        // var testParser = new Parser(parserPass, catalog);
+            //When
+            var testParser = new Parser(parserPass, catalog);
 
-        //Then
-        Assert.False(catalog.CategoryList.FirstOrDefault().Name.Contains('#'));
+            //Then
+            Assert.True(catalog.CategoryList.Count > 0);
+            Assert.False(catalog.CategoryList.FirstOrDefault().Name.Contains('#'));
         }
     }
 }
