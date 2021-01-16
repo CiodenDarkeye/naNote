877166b91e498f67b21afb2331579069adc946d9
CiodenDarkeye <Falcon2001@gmail.com>
2021-01-15 21:16:18 -0800

    Update Access.cs
    Fixed, now saves in the local data folder appropriately for windows.

--- naNote.Logic/Data/Access.cs
+++ naNote.Logic/Data/Access.cs
@@ -1,25 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System;
 using Newtonsoft.Json;
 
 namespace naNote.Logic.Data
 {
     public static class Access
     {
-        private static string jsonConfigFile = @"C:\Users\Will Jones\OneDrive\Code\c#\naNoteCLI\config.json";
+        private const string fileName = "naNoteDB.json";
+        private const string filePath = @"\naNote\";
+        private static string jsonSaveFile = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + filePath + fileName;
 
         public static void Save(Catalog catalogToSave)
         {
-            File.WriteAllText(jsonConfigFile, JsonConvert.SerializeObject(catalogToSave, Formatting.Indented));
+            // Start by checking whether or not the file actually exists
+            if (!File.Exists(jsonSaveFile))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + filePath);
+            }
+            File.WriteAllText(jsonSaveFile, JsonConvert.SerializeObject(catalogToSave, Formatting.Indented));
         }
 
         public static Catalog Load()
         {
-            return JsonConvert.DeserializeObject<Catalog>(File.ReadAllText(jsonConfigFile));
+            return JsonConvert.DeserializeObject<Catalog>(File.ReadAllText(jsonSaveFile));
         }
     }
 }
