6bcc9602bbddb97420e15990458c6dbecd31f919
CiodenDarkeye <Falcon2001@gmail.com>
2021-01-27 02:58:56 -0800

    Initial creation of GUI
    GUI created, right now it literally just does one thing, but it successfully makes a parser and a catalog.

---
+++ naNoteGUI/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using naNote.Logic.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace naNoteGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public Catalog Catalog;
+        public App()
+        {
+            
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Catalog = new Catalog();
+        }
+    }
+}
---
+++ naNoteGUI/naNoteHome.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using naNote.Logic;
+using naNote.Logic.Data;
+
+namespace naNoteGUI
+{
+    /// <summary>
+    /// Interaction logic for naNoteHome.xaml
+    /// </summary>
+    public partial class naNoteHome : Page
+    {
+        public naNoteHome()
+        {
+            InitializeComponent();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                var parsedText = new Parser(textBox1.Text, ((App)Application.Current).Catalog);
+                textBlock1.Text = $"Action: {parsedText.Action} \n" +
+                    $"Payload: {parsedText.Payload} \n" +
+                    $"Categories: {String.Join(", ",parsedText.Categories)}";
+                    
+            }
+        }
+    }
+}
